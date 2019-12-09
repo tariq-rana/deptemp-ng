03a380c1d8741efea89dd56f317928a8b53965a7
tariq-rana <tariq_rana@yahoo.com>
2019-12-09 11:50:18 +0700

    serialize javascript

--- src/app/services/dept.service.ts
+++ src/app/services/dept.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, Subject } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, Subject, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 import { DeptEntity } from '../entities/dept.entity';
 import { DeptDTO } from '../entities/dept.dto';
@@ -29,15 +30,41 @@ export class DeptService {
   }
 
   insertDept(deptDTO: DeptDTO): Observable<any> {
-    return this.http.post<DeptDTO>(`${this.apiServer}/dept`, deptDTO);
+    return this.http.post<DeptDTO>(`${this.apiServer}/dept`, deptDTO)
+    .pipe(
+      catchError(this.handleError)
+    );
   }
 
   updateDept(deptId:number, deptDTO: DeptDTO): Observable<any> {
-    return this.http.patch<DeptDTO>(`${this.apiServer}/dept/${deptId}`, deptDTO);
+    return this.http.patch<DeptDTO>(`${this.apiServer}/dept/${deptId}`, deptDTO)
+    .pipe(
+      catchError(this.handleError)
+    );
   }
 
   deleteDept(deptId:number){
-    return this.http.delete<any>(`${this.apiServer}/dept/${deptId}`);
+    return this.http.delete<any>(`${this.apiServer}/dept/${deptId}`)
+    .pipe(
+      catchError(this.handleError)
+    );
   }
+
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      'Something bad happened; please try again later.');
+  };
 
 }
--- src/app/services/emp.service.ts
+++ src/app/services/emp.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, Subject } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, Subject, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 import { EmpEntity } from '../entities/emp.entity';
 import { EmpDTO } from '../entities/emp.dto';
@@ -27,19 +28,47 @@ export class EmpService {
   constructor(private readonly http: HttpClient) { }
 
   findAllEmp(): Observable<EmpEntity[]> {
-    return this.http.get<EmpEntity[]>(`${this.apiServer}/emp`);
+    return this.http.get<EmpEntity[]>(`${this.apiServer}/emp`)
+    .pipe(
+      catchError(this.handleError)
+    )
   }
 
   insertEmp(empDTO: EmpDTO): Observable<any> {
-    return this.http.post<EmpDTO>(`${this.apiServer}/emp`, empDTO);
+    return this.http.post<EmpDTO>(`${this.apiServer}/emp`, empDTO)
+    .pipe(
+      catchError(this.handleError)
+    )
   }
 
   updateEmp(empId:number, empDTO: EmpDTO): Observable<EmpDTO> {
-    return this.http.patch<EmpDTO>(`${this.apiServer}/emp/${empId}`, empDTO);
+    return this.http.patch<EmpDTO>(`${this.apiServer}/emp/${empId}`, empDTO)    
+    .pipe(
+      catchError(this.handleError)
+    )
   }
 
   deleteEmp(empId:number){
-    return this.http.delete<any>(`${this.apiServer}/emp/${empId}`);
+    return this.http.delete<any>(`${this.apiServer}/emp/${empId}`)
+    .pipe(
+      catchError(this.handleError)
+    )
   }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      'Something bad happened; please try again later.');
+  };
 
 }
